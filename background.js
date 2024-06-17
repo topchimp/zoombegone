2d42796b39f069579581da322eb3adae0b2ca0f5
Scott Davies <sxdavies@gmail.com>
2024-06-17 11:53:33 +0100

    Update background.js
    change to base url

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
     chrome.tabs.onCreated.addListener(function(tab) {
         chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-                if(tab.url.startsWith("https://yourcompanyname.zoom.us") && changeInfo.status =="complete") {
+                if(tab.url.startsWith("https://zoom.us") && changeInfo.status =="complete") {
                     chrome.tabs.remove(tab.id);
                 }
         });
